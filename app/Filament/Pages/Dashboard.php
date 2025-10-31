0a5490aa27d4cf84d8d8fd636f667e5db0c2b581
dpfei <duanpengfe@163.com>
2025-10-31 22:20:36 +0800

    自定义仪表盘

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Filament\Pages;
+use Filament\Pages\Dashboard as BaseDashboard;
+
+class Dashboard extends BaseDashboard
+{
+    protected static ?string $navigationLabel = "仪表盘";
+    protected static ?string $title = "仪表盘";
+}
