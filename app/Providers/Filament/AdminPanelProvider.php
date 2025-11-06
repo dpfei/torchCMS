effbbae3fe13c0a03c2a836472b8c77463636d39
dpfei <duanpengfe@163.com>
2025-11-06 22:02:21 +0800

    将管理后台账户修改到admin表

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -49,6 +49,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->authGuard('admin');
     }
 }
